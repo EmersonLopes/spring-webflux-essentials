f5069b9e64f8fac28452041348f9bcc1f92fa789
Emerson Lopes <emersonsilvalopes1@gmail.com>
2021-08-08 18:02:30 -0300

    Criado primeiro endpoint para listar todos os animes.

---
+++ src/main/java/com/example/springwebfluxessentials/domain/Anime.java
@@ -0,0 +1,22 @@
+package com.example.springwebfluxessentials.domain;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@With
+@Table("anime")
+public class Anime {
+
+    @Id
+    private Integer id;
+    @NotEmpty(message = "The name of anime cannot be empty")
+    private String name;
+}
