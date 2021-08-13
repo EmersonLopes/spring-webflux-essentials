6bb3c7542474d7018ee9897e24c6c95f2414a2c6
Emerson Lopes <emersonsilvalopes1@gmail.com>
2021-08-13 10:25:36 -0300

    Endpoint AnimeService.findById criado.

--- src/main/java/com/example/springwebfluxessentials/repository/AnimeRepository.java
+++ src/main/java/com/example/springwebfluxessentials/repository/AnimeRepository.java
@@ -2,7 +2,11 @@ package com.example.springwebfluxessentials.repository;
 
 import com.example.springwebfluxessentials.domain.Anime;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Mono;
 
 
 public interface AnimeRepository extends ReactiveCrudRepository<Anime, Integer> {
+
+    @Override
+    Mono<Anime> findById(Integer integer);
 }
--- src/main/java/com/example/springwebfluxessentials/service/AnimeService.java
+++ src/main/java/com/example/springwebfluxessentials/service/AnimeService.java
@@ -3,8 +3,11 @@ package com.example.springwebfluxessentials.service;
 import com.example.springwebfluxessentials.domain.Anime;
 import com.example.springwebfluxessentials.repository.AnimeRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Service
 @RequiredArgsConstructor
@@ -15,5 +18,15 @@ public class AnimeService {
 
     public Flux<Anime> findAll() {
         return animeRepository.findAll();
+    }
+
+    public Mono<Anime> findById(Integer id) {
+        return animeRepository.findById(id)
+                .switchIfEmpty(monoResponseStatusNotFoundException())
+                .log();
+    }
+
+    public <T> Mono<T> monoResponseStatusNotFoundException(){
+        return Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND, "Anime not found"));
     }
 }
